015175c653d2177bec3c5cd09022c2296fd20cc3
Alex Huang <alex_huang@wistron.com>
2018-06-24 22:39:19 +0800

    image loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { HttpClientModule } from '@angular/common/http';
+import { IonicImageLoader } from 'ionic-image-loader';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +17,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    IonicImageLoader.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { ImageLoader } from 'ionic-image-loader';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  users = [];
+  jsonData = null;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private htppClient: HttpClient, private imageLoader: ImageLoader) {
 
+  }
+
+  loadData() {
+    if (!this.jsonData) {
+      this.htppClient.get('https://randomuser.me/api/?results=100').subscribe(res => {
+        this.users = res['results'];
+        this.jsonData = res['results'];
+      });
+    } else {
+      this.users = [];
+      setTimeout(() => {
+        this.users = this.jsonData; 
+      }, 1000);
+    }
+  }
+ 
+  clearCache(refresher) {
+    this.imageLoader.clearCache();
+    refresher.complete();
+  }
+ 
+  onImageLoad(event) {
+    console.log('image ready: ', event);
   }
 
 }
